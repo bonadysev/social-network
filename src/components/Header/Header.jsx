fcfd0ccfa61c5c31ae5db43948de65e53c1c3016
Sergey <bonadysev@gmail.com>
2022-02-04 22:09:28 +0300

    15 lessons start

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () =>{
+    return  <header className={s.header}>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5VOIJTKhQNgBLepC2ZrFqrEJ4s8fKh0FySbaKu-kQk9IHaRPOo8h6nQ6oglSQX4GxciM&usqp=CAU'/>
+    </header>
+}
+
+export default Header;
