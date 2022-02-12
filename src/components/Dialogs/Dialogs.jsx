227859b8a0ba5e30135af1ec947361399cfa8648
Sergey <bonadysev@gmail.com>
2022-02-12 17:02:42 +0300

    21. Dialogs typeset

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
-// import s from './Dialogs.module.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div className="dialogs">
-            <div class="dialogs-items">
-                <div className='item'>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
                     Ben
                 </div>
-                <div className='item'>
+                <div className={s.dialog}>
                     Fred
                 </div>
-                <div className='item'>
+                <div className={s.dialog}>
                     Cris
                 </div>
-                <div className='item'>
+                <div className={s.dialog}>
                     Bill
                 </div>
+                <div className={s.dialog}>
+                    Angelina
+                </div>
+                <div className={s.dialog}>
+                    Bred
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is you</div>
+                <div className={s.dialog}>Yo</div>
             </div>
         </div>
     )
