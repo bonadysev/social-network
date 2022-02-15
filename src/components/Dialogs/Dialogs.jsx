bf8d4a7ce9a284310117eba0c0087b070fa00758
Sergey <bonadysev@gmail.com>
2022-02-15 21:44:52 +0300

    25. React JS (map)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -15,7 +15,7 @@ const Message = (props) => {
 
 const Dialogs = () => {
 
-    let dialogsData = [
+    let dialogs = [
         {id:1, name: 'Ben'},
         {id:2, name: 'Fred'},
         {id:3, name: 'Cris'},
@@ -23,8 +23,7 @@ const Dialogs = () => {
         {id:5, name: 'Angelina'},
         {id:6, name: 'Bred'},
     ]
-
-    let messagesData = [
+    let messages = [
         {id:1, message: 'Hi'},
         {id:2, message: 'How is you it'},
         {id:3, message: 'Yo'},
@@ -32,18 +31,17 @@ const Dialogs = () => {
         {id:5, message: 'Yo'},
 
     ]
+    let dialogsElements = dialogs.map( (d) => <DialogItem name={d.name} id={d.id}/> )
+    let messagesElements = messages.map ((m)=><Message message={m.message}/>)
+
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-
+                {messagesElements}
             </div>
         </div>
     )
