a92d80a6f044b94ca51bee9d21640ea4929eb673
Sergey <bonadysev@gmail.com>
2022-03-07 13:36:59 +0300

    35. callback, subscribe, observer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,6 @@
-import {rerenderEntireTree} from "../render";
-
+let rerenderEntireTree = ()=> {
+    console.log('state changed')
+}
 let state = {
 
     profilePage:{
@@ -47,6 +48,10 @@ export let addPost = ()=> {
 export let updateNewPostText = (newText)=> {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer; // patern nabludatel (observer)
 }
 
 export default state;
