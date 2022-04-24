59e7697b691b70c23102d8e43b313573c2d6a6ae
Sergey <bonadysev@gmail.com>
2022-04-24 11:03:35 +0300

    39 - action creator, action type

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
 
@@ -57,6 +60,17 @@ let store = {
     }
 }
 
+export const addPostActionCreator = () => {
+    return {
+        type: ADD_POST
+    }
+}
+
+export const updateNewPostTextActionCreator = (text) => {
+    return {
+        type: UPDATE_NEW_POST_TEXT, newText: text
+    }
+}
 
 export default store;
 window.state = store;
