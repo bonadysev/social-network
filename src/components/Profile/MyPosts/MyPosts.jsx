c15afa829ddeb654c385ce4c880d8f162a0a07e5
Sergey <bonadysev@gmail.com>
2022-05-19 22:48:30 +0300

    43 container component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
-
 
 
 const MyPosts = (props) => {
-    let postsElements = props.posts.map((p)=><Post message={p.message} likesCount={p.likesCount}/>);
+    let postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount}/>);
 
     let newPostElement = React.createRef();
 
-    let addPost = ()=> {
-        props.dispatch( addPostActionCreator() );
+    let onAddPost = () => {
+        props.addPost();
     }
 
     let onPostChange = () => {
-    let text = newPostElement.current.value;
-        // let action = {type:'UPDATE-NEW-POST-TEXT',newText:text};
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text);
     }
 
     return (
@@ -29,7 +25,7 @@ const MyPosts = (props) => {
                     <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
                 </div>
                 <div>
-                    <button onClick={addPost}>Add post</button>
+                    <button onClick={onAddPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
