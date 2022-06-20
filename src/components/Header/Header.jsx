d106f4aeba2f2cb7ab403eb838f19a921ab3817c
Sergey <bonadysev@gmail.com>
2022-06-20 21:57:44 +0300

    61 - React JS - cookie, login, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () =>{
+const Header = (props) =>{
     return  <header className={s.header}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5VOIJTKhQNgBLepC2ZrFqrEJ4s8fKh0FySbaKu-kQk9IHaRPOo8h6nQ6oglSQX4GxciM&usqp=CAU'/>
+    <div className={s.loginBlock}>
+        {props.isAuth ? props.login
+        : <NavLink to={'/login'}>Login</NavLink>}
+    </div>
     </header>
 }
 
