26a3c2102a880eb2a5249e7cdb780654dcf40fb5
Sergey <bonadysev@gmail.com>
2022-07-04 19:37:34 +0300

    67 - redux-thunk

--- src/api/api.js
+++ src/api/api.js
@@ -1,31 +1,35 @@
 import * as axios from "axios";
 
 const instance = axios.create({
-    withCredentials:true,
-    baseURL:'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
         "API-KEY": "f5850288-e724-4ece-b82a-4d60962d11a0"
     }
 });
 
 export const usersAPI = {
-    getUsers(currentPage= 1 , pageSize = 10){
-        return  instance.get(`users?page=${currentPage}&count=${pageSize}`)
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => {
                 return response.data
             });
     },
-    follow(userId){
-        return  instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    follow(userId) {
+        return instance.post(`follow/${userId}`)
     },
-    unfollow(userId){
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    unfollow(userId) {
+        return instance.delete(`follow/${userId}`)
+    },
+    getProfile(userId) {
+        return instance.get(`profile/` + userId);
+
     }
 }
 
-export const getUsers2 = (currentPage= 1 , pageSize = 10)=> {
-    return  instance.get(`follow?page=${currentPage}&count=${pageSize}`)
-        .then(response => {
-            return response.data
-        });
+export const authAPI = {
+    me() {
+       return  instance.get(`auth/me`)
+    }
 }
+
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
-import {setUserProfile} from "../../redux/profile-reducer";
+import {getUserProfile} from "../../redux/profile-reducer";
 import {withCustomWithRouter} from "../../HOC/withCustomWithRouter";
+import {usersAPI} from "../../api/api";
 
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
-        const id = this.props.userId
-        // axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${id ? id : 2}`)
-            .then(response => {
-                this.props.setUserProfile(response.data);
-            });
+        let userId = this.props.userId
+        if (!userId){
+            userId = 2;
+        }
+        this.props.getUserProfile(userId);
     }
 
     render(){
@@ -30,4 +30,4 @@ let mapStateToProps = (state) => ({
 
 
 // export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
-export default connect(mapStateToProps, {setUserProfile})(withCustomWithRouter(ProfileContainer));
+export default connect(mapStateToProps, {getUserProfile})(withCustomWithRouter(ProfileContainer));
