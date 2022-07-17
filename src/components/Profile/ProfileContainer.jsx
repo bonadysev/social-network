b567d408b996c9be994c72a48115382941f1f3f7
Sergey <bonadysev@gmail.com>
2022-07-17 22:24:02 +0300

    73 - React JS - http put, local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Profile from "./Profile";
 import {connect} from "react-redux";
-import {getUserProfile} from "../../redux/profile-reducer";
+import {getStatus, getUserProfile, updateStatus} from "../../redux/profile-reducer";
 import {withCustomWithRouter} from "../../HOC/withCustomWithRouter";
 import {Navigate} from "react-router-dom";
 import {withAuthRedirect} from "../../HOC/withAuthRedirect";
@@ -13,24 +13,26 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.userId
         if (!userId){
-            userId = 2;
+            userId = 23644;
         }
         this.props.getUserProfile(userId);
+        this.props.getStatus(userId);
     }
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile}/>
+            <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus}/>
         )
     }
 }
 
 let mapStateToProps = (state) => ({
-    profile:state.profilePage.profile
+    profile:state.profilePage.profile,
+    status:state.profilePage.status
 })
 
 export default compose(
-    connect(mapStateToProps, {getUserProfile}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withCustomWithRouter
 )(ProfileContainer)
 
--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,8 +3,10 @@ import s from './ProfileInfo.module.css';
 
 
 class ProfileStatus extends React.Component {
+
     state = {
-        editMode: false
+        editMode: false,
+        status: !this.props.status
     }
     activateEditMode = () => {
 
@@ -14,23 +16,30 @@ class ProfileStatus extends React.Component {
         // this.forceUpdate();
     }
 
-    deactivateEditMode () {
+    deactivateEditMode = () => {
         this.setState({
             editMode:false
         })
+        this.props.updateStatus(this.state.status)
         // this.forceUpdate();
     }
+    onStatusChange = (e)=>{
+        this.setState({
+            status:e.currentTarget.value
+        })
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                 <div>
-                    <span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
+                    <span onDoubleClick={ this.activateEditMode }>{this.props.status || "no status"}</span>
                 </div>
                 }
                 {this.state.editMode &&
                 <div>
-                    <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                    <input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.status}/>
                 </div>
                 }
             </div>
