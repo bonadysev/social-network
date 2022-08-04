74b32ec0f7a8009b70c0f52ed67f0791d0c0a8b5
Sergey <bonadysev@gmail.com>
2022-08-04 17:13:46 +0300

    74 - React JS - componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -13,7 +13,7 @@ class ProfileStatus extends React.Component {
         this.setState({
             editMode:true
         })
-        // this.forceUpdate();
+
     }
 
     deactivateEditMode = () => {
