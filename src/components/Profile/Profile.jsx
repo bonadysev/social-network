eb0dd07b04713385289614d1c302d9c3a394cfa4
Sergey <bonadysev@gmail.com>
2022-08-10 10:24:00 +0300

    74 - React JS - componentDidUpdate

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
-
 const Profile = (props) => {
     return (
         <div>
